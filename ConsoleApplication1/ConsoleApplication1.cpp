51059e50c53bc43b3c752792687607f864e3ac99
Ecstasy317 <108227153+Ecstasy317@users.noreply.github.com>
2023-01-21 23:28:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	srand(time(0));
+	int rand_numb = rand() % 6 + 1;
+	int my_numb;
+	cout << "Enter a number from 1 to 6: ";
+	cin >> my_numb;
+	if (my_numb <= 0 || my_numb > 6)
+	{
+		cout << "\nYour number doesn't fit!\n";
+		return 0;
+	}
+	if (rand_numb == my_numb)
+	{
+		cout << "You won!\n";
+	}
+	if (rand_numb != my_numb)
+	{
+		cout << "You lose!\n";
+		cout << "There was a number: " << rand_numb << "\t";
+	}
+}
